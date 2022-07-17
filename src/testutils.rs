b012e96abd47845766e045657119bab17e4c2a06
William Findlay <will@isovalent.com>
2022-07-17 02:11:15 -0400

    tests: add report serialize test
    Signed-off-by: William Findlay <will@isovalent.com>

--- src/testutils.rs
+++ src/testutils.rs
@@ -8,27 +8,42 @@
 //! Utility functions for writing Houdini unit tests.
 
 #![cfg(test)]
+#![allow(dead_code)]
 
 use std::fmt::Debug;
 
 use serde::{de::DeserializeOwned, Serialize};
-use serde_yaml::{from_str, to_string};
 
 /// Deserialize a yaml string, serialize it back, and deserialize it again ensuring the
 /// two resulting objects are equal and returning the first object.
 pub fn assert_yaml_deserialize<T: Serialize + DeserializeOwned + Debug + Eq>(yaml: &str) -> T {
-    let obj1: T = from_str(yaml).expect("should deserialize");
-    let yaml = to_string(&obj1).expect("should serialize");
-    let obj2: T = from_str(&yaml).expect("should deserialize back");
-    assert_eq!(obj1, obj2, "deserialized structs should be the same");
+    let obj1: T = serde_yaml::from_str(yaml).expect("should deserialize");
+    assert_yaml_serialize(&obj1);
+    obj1
+}
+
+/// Serialize a T as a yaml string, and deserialize it back ensuring the two
+/// resulting objects are equal and returning the yaml string.
+pub fn assert_yaml_serialize<T: Serialize + DeserializeOwned + Debug + Eq>(obj: &T) -> String {
+    let yaml = serde_yaml::to_string(obj).expect("should serialize");
+    let obj2: T = serde_yaml::from_str(&yaml).expect("should deserialize back");
+    assert_eq!(obj, &obj2, "deserialized structs should be the same");
+    yaml
+}
+
+/// Deserialize a json string, serialize it back, and deserialize it again ensuring the
+/// two resulting objects are equal and returning the first object.
+pub fn assert_json_deserialize<T: Serialize + DeserializeOwned + Debug + Eq>(json: &str) -> T {
+    let obj1: T = serde_json::from_str(json).expect("should deserialize");
+    assert_json_serialize(&obj1);
     obj1
 }
 
 /// Serialize a T as a json string, and deserialize it back ensuring the two
-/// resulting objects are equal and returning json string.
+/// resulting objects are equal and returning the json string.
 pub fn assert_json_serialize<T: Serialize + DeserializeOwned + Debug + Eq>(obj: &T) -> String {
     let json = serde_json::to_string(obj).expect("should serialize");
-    let obj2: T = from_str(&json).expect("should deserialize back");
+    let obj2: T = serde_json::from_str(&json).expect("should deserialize back");
     assert_eq!(obj, &obj2, "deserialized structs should be the same");
     json
 }
