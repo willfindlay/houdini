23004d8aaca1527c92be9a72e3695f62854ca3d3
William Findlay <will@isovalent.com>
2022-08-06 12:31:29 -0400

    api: add connection logging via middleware
    Signed-off-by: William Findlay <will@isovalent.com>

---
+++ src/api/middleware.rs
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// Houdini  A container escape artist
+// Copyright (c) 2022  William Findlay
+//
+// February 25, 2022  William Findlay  Created this.
+//
+
+//! Middleware for the Houdini API.
+
+use crate::api::uds::UdsConnectInfo;
+use axum::{
+    extract::{ConnectInfo, RequestParts},
+    http::Request,
+    middleware::Next,
+    response::Response,
+};
+
+pub async fn log_connection<B>(request: Request<B>, next: Next<B>) -> Response
+where
+    B: Send,
+{
+    let mut parts = RequestParts::new(request);
+
+    match parts.extract::<ConnectInfo<UdsConnectInfo>>().await {
+        Ok(info) => tracing::info!("new connection from {:?}", info),
+        Err(e) => tracing::warn!(err = ?e, "failed to extract connection info"),
+    };
+
+    let request = parts.try_into_request().expect("body extracted");
+    next.run(request).await
+}
